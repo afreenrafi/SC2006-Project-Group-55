8b72e544d8d02ba77dc1dd16e4c600dcddc82f6b
gwenionna <gwen.ionna@gmail.com>
2024-10-03 15:55:06 +0800

    Added MockData

---
+++ frontend/app/tabs/mockData.jsx
@@ -0,0 +1,57 @@
+export const mockUpcomingEvents = [
+    {
+      id: '1',
+      name: 'Hello Future: Building a Wonderland',
+      type: 'Exhibition',
+      date: '01 Dec 2024',
+      location: 'Singapore Arts Museum',
+      image: require('../../assets/events/helloFuture.jpg'),
+    },
+    {
+      id: '2',
+      name: 'Sea Chanty Project',
+      type: 'Exhibition',
+      date: '05 Dec 2024',
+      location: 'Singapore Arts Museum',
+      image: require('../../assets/events/seaChantyProject.jpg'),
+    },
+  ];
+  
+  export const mockPopularEvents = [
+    {
+      id: '1',
+      name: 'Singapore Night Festival',
+      type: 'Festival',
+      date: '10 Dec 2024',
+      location: 'Singapore Night Festival',
+      image: require('../../assets/events/SNF.png'),
+    },
+    {
+      id: '2',
+      name: 'Hello Future: Building a Wonderland',
+      type: 'Exhibition',
+      date: '12 Dec 2024',
+      location: 'Singapore Arts Museum',
+      image: require('../../assets/events/helloFuture.jpg'),
+    },
+  ];
+  
+  export const mockNearbyEvents = [
+    {
+      id: '1',
+      name: 'Singapore Night Festival',
+      type: 'Festival',
+      date: '10 Dec 2024',
+      location: 'Singapore Night Festival',
+      image: require('../../assets/events/SNF.png'),
+    },
+    {
+      id: '2',
+      name: 'Hello Future: Building a Wonderland',
+      type: 'Exhibition',
+      date: '01 Dec 2024',
+      location: 'Singapore Arts Museum',
+      image: require('../../assets/events/helloFuture.jpg'),
+    },
+  ];
+  
