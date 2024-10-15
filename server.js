0f2583e9ba6dac169f3d087bf5369a66e71b77fc
afreen rafi <afreenrafi151@gmail.com>
2024-10-15 16:37:10 +0800

    changed mongo to connect to port

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use("/api/userRoutes", userRoutes); // Handle user routes (update, delete)
 const PORT = process.env.PORT || 5000; // Default port
 
 mongoose
-	.connect(process.env.MONGO_URL)
+	.connect(process.env.MONGO_URI)
 	.then(() => {
 		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 	})
