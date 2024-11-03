093a11581796f8ebb3aa1be677d8033e3043ca10
Adolphus Chua <160819329+ggeojyeo@users.noreply.github.com>
2024-11-04 03:20:59 +0800

    - created eventTicket.js
    - created eventTicketController.js
    
    - created eventTicketRoute.js
    
    - edited app.js to include eventTicketRoute.js
    
    - edited Event.js to reference eventTicket.js
    
    - edited eventController.js to include eventTicket.js attribute
    
    - edited userController.js comments

---
+++ backend/routes/eventTicketRoute.js
@@ -0,0 +1,29 @@
+// IMPORT NECESSARY LIBRARIES
+import express from "express";
+import {
+  createEventTicket,
+  getAllEventTickets,
+  getEventTicketById,
+  updateEventTicket,
+  deleteEventTicket,
+} from "../controllers/eventTicketController.js";
+
+// INSTANTIATE ROUTER
+const router = express.Router();
+
+// DEFINE ROUTES FOR EVENTTICKET ENTITY
+
+// CREATE
+router.post("/", createEventTicket);
+
+// READ
+router.get("/", getAllEventTickets);
+router.get("/:eventTicketId", getEventTicketById); 
+
+// UPDATE
+router.put("/:eventTicketId", updateEventTicket);
+
+// DELETE
+router.delete("/:eventTicketId", deleteEventTicket); 
+
+export default router;
